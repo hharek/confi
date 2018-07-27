44e87943ecd89891afbba0b57d500e3d40c7b95c
harek <harek@hcomp.localhost>
2018-07-27 18:37:57 +0300

    Удалены ненужные #pragma once

--- inc/value.h
+++ inc/value.h
@@ -1,5 +1,3 @@
-#pragma once
-
 static int value_set (struct confi_param * param);
 static int value_set_int (const char * name, const char * value, void * ptr);
 static int value_set_uint (const char * name, const char * value, void * ptr);
